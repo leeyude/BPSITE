fa2c6778f90f2d2c99d541a7ddb3c996a19d6935
leeyude-mac <leeyude@umich.ede>
2015-08-16 15:09:25 -0400

    upload complete

--- client/template/workstation/zipClient.js
+++ client/template/workstation/zipClient.js
@@ -3,50 +3,53 @@ Zips= new Mongo.Collection("zips");
 
 Template.zipcodes.helpers ({
   zipLine: function(){
-    return Zips.find();
+    return Zips.find({},{limit : 100});
   },
 });
 
 Template.zipcodes.events({
- 'click #readCSV': function(event) {
-console.log("click");
-document.getElementById('readCSV').addEventListener('click', readFile, false);
-var files = event.target.files;
-var loadedFile = document.getElementById('readCSV').files[0];
-
-console.log("read file");
-readFile(loadedFile, function(content) {
-Meteor.call('upload',content);
-});
- }
+  'click #readCSV': function(event) {
+    event.preventDefault();
+    var reader = new FileReader();
+    var loadedFile = document.getElementById('readZipCSV').files[0];
+    console.log(loadedFile);
+    readFile(loadedFile, function(content) {
+      Meteor.call('upload',content);
+    })
+  }
 });
 
-import_file_CSV = function(file) {
-  console.log("enter function import_file_orders")
-  var lines = file.split(/\r\n|\n/);
-  var l = lines.length - 1;
-  for (var i=0; i < l; i++) {
-    var line = lines[i];
-    var line_parts = line.split(',');
-
-    var zipcode = line_parts[0];
-    var cityName = line_parts[1];
-    var stateAbb = line_parts[2];
-    var currentServing = "N";
-
-    var result = Zips.insert({zipcode:zipcode, cityName:cityName, stateAbb:stateAbb});
-    console.log(Zips.findOne(result));
-    };
-    return result;
-}
-
- readFile = function(loadedFile,onLoadCallback) {
+readFile = function(loadedFile,onLoadCallback) {
 //When the file is loaded the callback is called with the contents as a string
   var reader = new FileReader();
-  reader.onload = function (e){
-  var contents=e.target.result;
+  reader.onload = function (event){
+    var contents=event.target.result;
     onLoadCallback(contents);
   }
   reader.readAsText(loadedFile);
-  return false;
 };
+
+
+/*
+ var data;
+
+ function handleFileSelect(evt) {
+   var file = evt.target.files[0];  //loadedFile
+
+   Papa.parse(file, {
+     header: false,
+     dynamicTyping: true,
+     complete: function(data) {
+       console.log(data);
+       Meteor.call('upload',data);
+     }
+   });
+ }
+
+
+ $(document).ready(function(){
+   $("#readZipCSV").change(handleFileSelect);
+ });
+
+});
+*/
--- server/zipsServer.js
+++ server/zipsServer.js
@@ -1,9 +1,35 @@
 Zips= new Mongo.Collection("zips");
 
 Meteor.methods({
-  upload : function(fileContent) {
-  console.log("start insert");
-  import_file_CSV(fileContent);
-  console.log("completed");
-}
+   upload : function(fileContent) {
+   console.log("start insert");
+   import_file_CSV(fileContent);
+   console.log("completed");
+ }
 });
+
+import_file_CSV = function(file) {
+  console.log("enter function")
+  var lines = file.split(/\r\n|\n/);
+  var l = lines.length - 1;
+  for (var i=0; i < l; i++) {
+    var line = lines[i];
+    var line_parts = line.split(',');
+    if(line_parts[0].length===3){
+      var zipcode = "00"+line_parts[0];
+    } else {
+      if(line_parts[0].length===4){
+        var zipcode = "0"+line_parts[0];
+      }else{
+        var zipcode = line_parts[0];
+      };
+    };
+
+    var cityName = line_parts[1];
+    var stateAbb = line_parts[2];
+    var currentServing = "No";
+
+    var result = Zips.insert({zipcode:zipcode, cityName:cityName, stateAbb:stateAbb, currentServing: currentServing});
+    };
+    return result;
+}
